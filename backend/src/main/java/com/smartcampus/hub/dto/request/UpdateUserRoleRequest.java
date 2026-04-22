0bf480ddd02d8ff716d08c3bd9911a7bcc5e384c
Piriyankan <piriyankanr45@gmail.com>
2026-04-22 14:28:26 +0530

    Update user role request

--- backend/src/main/java/com/smartcampus/hub/dto/request/UpdateUserRoleRequest.java
+++ backend/src/main/java/com/smartcampus/hub/dto/request/UpdateUserRoleRequest.java
@@ -1,12 +1,12 @@
-// package com.smartcampus.hub.dto.request;
+package com.smartcampus.hub.dto.request;
 
-// import com.smartcampus.hub.enums.Role;
-// import jakarta.validation.constraints.NotNull;
-// import lombok.Data;
+import com.smartcampus.hub.enums.Role;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-// @Data
-// public class UpdateUserRoleRequest {
+@Data
+public class UpdateUserRoleRequest {
 
-//     @NotNull(message = "Role is required")
-//     private Role role;
-// }
+    @NotNull(message = "Role is required")
+    private Role role;
+}
