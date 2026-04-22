4cffaa4b59015f139987f4f4f298bc94028fc44d
Piriyankan <piriyankanr45@gmail.com>
2026-04-22 14:38:50 +0530

    Activity log response

--- backend/src/main/java/com/smartcampus/hub/dto/response/ActivityLogResponse.java
+++ backend/src/main/java/com/smartcampus/hub/dto/response/ActivityLogResponse.java
@@ -1,19 +1,19 @@
-// package com.smartcampus.hub.dto.response;
+package com.smartcampus.hub.dto.response;
 
-// import lombok.AllArgsConstructor;
-// import lombok.Builder;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-// @Data
-// @Builder
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class ActivityLogResponse {
-//     private Long id;
-//     private String action;
-//     private String details;
-//     private LocalDateTime timestamp;
-// }
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityLogResponse {
+    private Long id;
+    private String action;
+    private String details;
+    private LocalDateTime timestamp;
+}
