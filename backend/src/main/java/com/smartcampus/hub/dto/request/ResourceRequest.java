e94aec8e880730a07086804205eaefc0d7dd4b2e
nithar <nitharshijr@gmail.com>
2026-04-23 14:15:03 +0530

    resource req

--- backend/src/main/java/com/smartcampus/hub/dto/request/ResourceRequest.java
+++ backend/src/main/java/com/smartcampus/hub/dto/request/ResourceRequest.java
@@ -1,19 +1,19 @@
-// package com.smartcampus.hub.dto.request;
+ package com.smartcampus.hub.dto.request;
 
-// import com.smartcampus.hub.enums.ResourceStatus;
-// import jakarta.validation.constraints.Min;
-// import jakarta.validation.constraints.NotBlank;
-// import jakarta.validation.constraints.NotNull;
-// import lombok.Data;
+ import com.smartcampus.hub.enums.ResourceStatus;
+ import jakarta.validation.constraints.Min;
+ import jakarta.validation.constraints.NotBlank;
+ import jakarta.validation.constraints.NotNull;
+ import lombok.Data;
 
-// @Data
-// public class ResourceRequest {
+ @Data
+ public class ResourceRequest {
 
-//     @NotBlank(message = "Name is required")
-//     private String name;
+     @NotBlank(message = "Name is required")
+     private String name;
 
-//     @NotBlank(message = "Type is required")
-//     private String type;
+     @NotBlank(message = "Type is required")
+     private String type;
 
 //     @NotNull(message = "Capacity is required")
 //     @Min(value = 1, message = "Capacity must be at least 1")
