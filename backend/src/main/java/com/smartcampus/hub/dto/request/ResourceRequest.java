cbf50446cec611037019590c3e590e388c38fb28
nithar <nitharshijr@gmail.com>
2026-04-23 14:20:02 +0530

    resource request

--- backend/src/main/java/com/smartcampus/hub/dto/request/ResourceRequest.java
+++ backend/src/main/java/com/smartcampus/hub/dto/request/ResourceRequest.java
@@ -15,20 +15,20 @@
      @NotBlank(message = "Type is required")
      private String type;
 
-//     @NotNull(message = "Capacity is required")
-//     @Min(value = 1, message = "Capacity must be at least 1")
-//     private Integer capacity;
+    @NotNull(message = "Capacity is required")
+     @Min(value = 1, message = "Capacity must be at least 1")
+     private Integer capacity;
 
-//     @NotBlank(message = "Location is required")
-//     private String location;
+     @NotBlank(message = "Location is required")
+     private String location;
 
-//     @NotBlank(message = "Availability window is required")
-//     private String availabilityWindow;
+     @NotBlank(message = "Availability window is required")
+     private String availabilityWindow;
 
-//     @NotNull(message = "Status is required")
-//     private ResourceStatus status;
+     @NotNull(message = "Status is required")
+     private ResourceStatus status;
 
-//     private String startDate;
-//     private String endDate;
-//     private Boolean everyDay;
-// }
+     private String startDate;
+     private String endDate;
+     private Boolean everyDay;
+ }
