6833bd0eff53d24df107a3aaf02bf95233e08bb5
Aswini <saravananashwini2003@gmail.com>
2026-04-24 12:52:52 +0530

    TicketAttachmentRepository

--- backend/src/main/java/com/smartcampus/hub/repository/TicketAttachmentRepository.java
+++ backend/src/main/java/com/smartcampus/hub/repository/TicketAttachmentRepository.java
@@ -1,12 +1,12 @@
-// package com.smartcampus.hub.repository;
+package com.smartcampus.hub.repository;
 
-// import com.smartcampus.hub.entity.TicketAttachment;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import com.smartcampus.hub.entity.TicketAttachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// import java.util.List;
+import java.util.List;
 
-// @Repository
-// public interface TicketAttachmentRepository extends JpaRepository<TicketAttachment, Long> {
-//     List<TicketAttachment> findByTicketId(Long ticketId);
-// }
+@Repository
+public interface TicketAttachmentRepository extends JpaRepository<TicketAttachment, Long> {
+    List<TicketAttachment> findByTicketId(Long ticketId);
+}
